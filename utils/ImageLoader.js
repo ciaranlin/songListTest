6bc70ace68c8ea0dd7ebc9a53f139ea28d3af314
Ciaran <onesonglin@gmail.com>
2025-12-23 16:38:21 +0800

    banner_

--- utils/ImageLoader.js
+++ utils/ImageLoader.js
@@ -1,4 +1,13 @@
 function imageLoader({ src }) {
+  if (!src) return src;
+
+  // 完整 URL 直接返回
+  if (src.startsWith("http://") || src.startsWith("https://")) return src;
+
+  // 已经是绝对路径，别再加 /
+  if (src.startsWith("/")) return src;
+
+  // 否则补一个 /
   return `/${src}`;
 }
 
